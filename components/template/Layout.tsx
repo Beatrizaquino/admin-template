ac6f08371dea81ee4086e05be528ffea0ed69411
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-04-03 16:58:08 -0400

    add divisor

--- components/template/Layout.tsx
+++ components/template/Layout.tsx
@@ -1,6 +1,6 @@
 import Conteudo from "./Conteudo"
 import MenuLateral from "./MenuLateral"
-import Cabecalho from "./cabecalho"
+import Cabecalho from "./Cabecalho"
 
 interface LayoutProps {
     titulo: string
@@ -10,12 +10,17 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
     return (
-        <div >
+        <div className={`flex h-screen w-screen`} >
             <MenuLateral />
+            <div className={`
+            flex dlex-col w-full p-7
+            bg-gray-300`
+            }> 
             <Cabecalho  titulo={props.titulo} subtitulo={props.subtitulo}/>
             <Conteudo>
                 {props.children}
             </Conteudo>
+            </div>
         </div>
     )
 }
