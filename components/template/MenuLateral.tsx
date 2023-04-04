86764368f3ffddde36f2c0f1bbd3610bd0f0c759
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-04-04 14:49:23 -0400

    componente menu lateral

--- components/template/MenuLateral.tsx
+++ components/template/MenuLateral.tsx
@@ -1,9 +1,16 @@
+import MenuItem from "./MenuItems";
+import { IconAlert, IconCode, IconHome, IconSetting } from "./icons";
 
 
 export default function MenuLateral() {
     return (
         <aside>
-            <h1>Menu</h1>
+            <ul className={``}>
+                <MenuItem url="/" texto="Home" icone={IconHome}/>
+                <MenuItem url="/"  texto="Settings" icone={IconSetting}/>
+                <MenuItem url="/"  texto="Alert" icone={IconAlert}/>
+                <MenuItem url="/"  texto="Study" icone={IconCode}/>
+            </ul>
         </aside>
     )
 }
